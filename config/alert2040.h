13005d0d0aed05732da284caebc38f00f9283226
CrazyBolillo <antonio@zoftko.com>
2023-12-02 14:47:29 -0600

    feat: implement ultrasonic sensor and monitor tasks
    A task and IRQ have been implemented to measure distance with an
    ultrasonic sensor. A monitor task in order to act on the sensor readings
    has been implemented as well.

--- config/alert2040.h
+++ config/alert2040.h
@@ -3,8 +3,12 @@
 
 #define LED_ERROR 16
 #define LED_WARNING 17
-#define LED_GROUP_MASK ((1 << LED_ERROR) | (1 << LED_WARNING))
+#define LED_PROXIMITY 15
+#define LED_GROUP_MASK ((1 << LED_ERROR) | (1 << LED_WARNING) | (1 << LED_PROXIMITY))
 
 #define ADC_PIN 28
+
+#define TRIG_PIN 9
+#define ECHO_PIN 8
 
 #endif  // ALERT2040_ALERT2040_H
